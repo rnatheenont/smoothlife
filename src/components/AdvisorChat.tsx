8221df16b4ef31fc3f4b69d1922f2fb67cfb58a0
User <User@Natheenont-UXUI.local>
2026-08-05 14:03:42 +0700

    Replace project files

--- src/components/AdvisorChat.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-// Superseded by QuickChat (floating widget). Kept as a thin alias.
-import QuickChat from "@/components/QuickChat";
-
-export default function AdvisorChat() {
-  return <QuickChat />;
-}
