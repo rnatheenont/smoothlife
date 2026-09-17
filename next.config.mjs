292f60ae298dc3c934080aba80b1fb7fbafcd0aa
User <User@Natheenont-UXUI.local>
2026-09-17 09:59:39 +0700

    next.config: drop eslint key (not supported in 16)

--- next.config.mjs
+++ next.config.mjs
@@ -46,7 +46,6 @@ const CSP = [
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: false },
   typescript: { ignoreBuildErrors: false },
   images: {
     remotePatterns: [
